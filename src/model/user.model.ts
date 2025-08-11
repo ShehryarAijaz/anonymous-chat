76e4571f612a2fa21affb9cd0d5eca93bdf58650
Shehryar Aijaz <shehryar120345@gmail.com>
2025-08-11 06:11:48 +0500

    fix: Rename 'message' to 'messages' in User model for consistency

--- src/model/user.model.ts
+++ src/model/user.model.ts
@@ -25,7 +25,7 @@ export interface User extends Document {
     verifyCodeExpires: Date;
     isVerified: boolean;
     isAcceptingMessages: boolean;
-    message: Message[]; 
+    messages: Message[]; 
 }
 
 const userSchema: Schema<User> = new Schema({
@@ -60,7 +60,7 @@ const userSchema: Schema<User> = new Schema({
         type: Boolean,
         default: false
     },
-    message: [messageSchema]
+    messages: [messageSchema]
 })
 
 const UserModel = (mongoose.models.User as mongoose.Model<User>) || mongoose.model<User>("User", userSchema)
