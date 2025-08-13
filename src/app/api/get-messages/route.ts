ccb1aafa3d430ed9551af6ee9c623b3186f26ae0
Shehryar Aijaz <shehryar120345@gmail.com>
2025-08-14 03:51:39 +0500

    feat: Implement GET API endpoint to fetch user messages with authentication and error handling

---
+++ src/app/api/get-messages/route.ts
@@ -0,0 +1,69 @@
+import dbConnect from "@/lib/dbConnect";
+import UserModel from "@/model/user.model";
+import { auth } from "@/auth";
+import { User } from "next-auth";
+import mongoose from "mongoose";
+
+export async function GET(request: Request) {
+    await dbConnect()
+
+    const userSession = await auth()
+    const user = userSession?.user as User
+
+    if (!userSession || !user) {
+        return Response.json({
+            success: false,
+            message: "Unauthorized"
+        }, {
+            status: 401
+        })
+    }
+
+    const userId = new mongoose.Types.ObjectId(user._id)
+
+    try {
+        const user = await UserModel.aggregate([
+            {
+                $match: { _id: userId }
+            },
+            {
+                $unwind: "$messages"
+            },
+            {
+                $sort: { "messages.createdAt": -1 }
+            },
+            {
+                $group: { _id: "$_id", messages: { $push: "$messages" } }
+            }
+        ])
+
+        if (!user || user.length === 0) {
+            return Response.json({
+                success: false,
+                message: "User not found"
+            }, {
+                status: 404
+            })
+        }
+
+        return Response.json({
+            success: true,
+            message: "Messages fetched successfully",
+            data: user[0].messages
+        }, {
+            status: 200
+        })
+        
+    } catch (error) {
+        console.error("Failed to get messages", error)
+
+        return Response.json({
+            success: false,
+            message: "Failed to get messages"
+        }, {
+            status: 500
+        })
+    }
+
+    
+}
