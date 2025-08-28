74bd756f668837bad3f1d137c0d78c216ef1a30d
Shehryar Aijaz <shehryar120345@gmail.com>
2025-08-28 06:16:57 +0500

    feat: Initialize sing-in page

--- src/app/(auth)/sign-in/page.tsx
+++ src/app/(auth)/sign-in/page.tsx
@@ -1,23 +1,9 @@
-'use client'
 import React from 'react'
-import { useSession, signOut, signIn } from 'next-auth/react'
 
-const Page = () => {
-    const { data: session } = useSession()
-    if (session) {
-      return (
-        <>
-        Signed in as {session.user?.email} <br />
-        <button onClick={() => signOut()}>Sign out</button>
-        </>
-      )
-    }
-    return (
-        <>
-        Not signed in <br />
-        <button onClick={() => signIn()}>Sign in</button>
-        </>
-    )
+const page = () => {
+  return (
+    <div>page</div>
+  )
 }
 
-export default Page
+export default page
