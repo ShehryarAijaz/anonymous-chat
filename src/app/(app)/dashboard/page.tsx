25f4e12115c9a6da1585346130231ac53fb16dfb
Shehryar Aijaz <shehryar120345@gmail.com>
2025-09-02 00:22:38 +0500

    feat: Enhance DashboardPage with message fetching, user settings, and clipboard functionality

--- src/app/(app)/dashboard/page.tsx
+++ src/app/(app)/dashboard/page.tsx
@@ -1,9 +1,197 @@
-import React from 'react'
+"use client";
+import MessageCard from "@/components/shared/MessageCard";
+import { Button } from "@/components/ui/button";
+import { Separator } from "@/components/ui/separator";
+import { Switch } from "@/components/ui/switch";
+import { Message, User } from "@/model/user.model";
+import { acceptMessageSchema } from "@/schemas/acceptMessage.schema";
+import { ApiResponse } from "@/types/ApiResponse";
+import { zodResolver } from "@hookform/resolvers/zod";
+import axios, { AxiosError } from "axios";
+import { Loader2, RefreshCcw } from "lucide-react";
+import { useSession } from "next-auth/react";
+import { useCallback, useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
 
 const DashboardPage = () => {
+  const [messages, setMessages] = useState<Message[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isSwitchLoading, setIsSwitchLoading] = useState<boolean>(false);
+
+  // This function gets a messageId and filter out the messages from the array of messages by removing the message whose id matches with the id provided
+  const handleDeleteMessage = (messageId: string) => {
+    setMessages(messages.filter((message) => message._id !== messageId));
+  };
+
+  const { data: session } = useSession();
+  console.log("Session:", session);
+  const user = session?.user as User;
+
+  // Initialize form that only accepts form that gets resolved by acceptMessageSchema
+  const form = useForm({
+    resolver: zodResolver(acceptMessageSchema),
+  });
+
+  // Destructuring register, watch and setValue from form
+  const { register, watch, setValue } = form;
+
+  // Watching the state of acceptMessages from form and storing in acceptMessages
+  const acceptMessages = watch("acceptMessages");
+
+  // Fetch state of user if they're accepting the messages or not. If they do, set the value of acceptMessages to value from the backend
+  const fetchAcceptMessage = useCallback(async () => {
+    setIsSwitchLoading(true);
+
+    try {
+      const response = await axios.get<ApiResponse>("/api/accept-messages");
+      setValue("acceptMessages", response.data.isAcceptingMessages ?? false);
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast.error(axiosError.response?.data.message ?? "Something went wrong");
+    } finally {
+      setIsSwitchLoading(false);
+    }
+  }, [setValue]);
+
+  // Fetch all the messages of the user from the backend, and store them in the messages array
+  const fetchMessages = useCallback(
+    async (refresh: boolean = false) => {
+      setIsLoading(true);
+      setIsSwitchLoading(false);
+
+      try {
+        const response = await axios.get("/api/get-messages");
+        if (response.data.success) {
+          setMessages(response.data.messages ?? []);
+          if (refresh) {
+            toast.success(
+              response.data.message ?? "Messages fetched successfully"
+            );
+          }
+        } else {
+          toast.error(response.data.message ?? "Failed to fetch messages");
+        }
+      } catch (error) {
+        const axiosError = error as AxiosError<ApiResponse>;
+        toast.error(
+          axiosError.response?.data.message ?? "Something went wrong!"
+        );
+      } finally {
+        setIsLoading(false);
+        setIsSwitchLoading(false);
+      }
+    },
+    [setIsLoading, setMessages]
+  );
+
+  // Run this hook that contains core functions whenever the state of session, setValue, fetchAcceptMessage and fetchMessages
+  useEffect(() => {
+    if (!session || !session.user) return;
+    fetchAcceptMessage();
+    fetchMessages();
+  }, [session, setValue, fetchAcceptMessage, fetchMessages]);
+
+  // Change the state of the acceptMessages in the backend whenever user toggles the switch
+  const handleSwitchChange = async () => {
+    try {
+      const response = await axios.post<ApiResponse>("/api/accept-messages", {
+        acceptMessages: !acceptMessages,
+      });
+
+      if (response.data.success) {
+        setValue("acceptMessages", !acceptMessages);
+        toast.success(response.data.message);
+      } else {
+        toast.error(response.data.message);
+      }
+    } catch (error) {
+      const axiosError = error as AxiosError<ApiResponse>;
+      toast.error(axiosError.response?.data.message ?? "Something went wrong");
+    } finally {
+      setIsSwitchLoading(false);
+    }
+  };
+
+  // Destructure username from user object
+  const { username } = session?.user as User;
+
+  const baseUrl = `${window.location.protocol}//${window.location.host}`;
+  const profileUrl = `${baseUrl}/u/${username}`;
+
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(profileUrl);
+    toast.success("Copied to clipboard!");
+  };
+
+  if (!session || !session.user)
+    return (
+      <div>
+        Loading...<Loader2></Loader2>
+      </div>
+    );
+
   return (
-    <div>Dashboard</div>
-  )
-}
+    <div className="my-8 mx-4 md:mx-8 lg:mx-auto p-6 bg-white rounded w-full max-w-6xl">
+      <h1 className="text-4xl font-bold mb-4">Anonymous Chat</h1>
+      <div className="mb-4">
+        <h2 className="text-lg font-semibold mb-2">Copy Your Unique Link</h2>
+        <div className="flex items-center">
+          <input
+            type="text"
+            value={profileUrl}
+            disabled
+            className="input input-bordered w-full p-2 mr-2"
+          />
+          <Button onClick={() => copyToClipboard}>Copy</Button>
+        </div>
+      </div>
+
+      <div className="mb-4">
+        <Switch
+          {...register("acceptMessages")}
+          checked={acceptMessages}
+          onCheckedChange={handleSwitchChange}
+          disabled={isSwitchLoading}
+        />
+        <span className="ml-2">
+          Accept Messages: {acceptMessages ? "On" : "Off"}
+        </span>
+      </div>
+      <Separator />
+
+      <Button
+        className="mt-4"
+        variant="outline"
+        onClick={(e) => {
+          e.preventDefault();
+          fetchMessages(true);
+        }}
+      >
+        {isLoading ? (
+          <Loader2 className="h-4 w-4 animate-spin" />
+        ) : (
+          <RefreshCcw className="h-4 w-4" />
+        )}
+      </Button>
+
+      <div className="mt-4 grid grid-cols-1 md:grid-cols-2 gap-6">
+        {messages.length > 0 ? (
+          messages.map((message) => (
+            <MessageCard
+              key={message._id as string}
+              message={message}
+              onMessageDelete={handleDeleteMessage}
+            />
+          ))
+        ) : (
+          <div className="col-span-2">
+            <p className="text-center text-gray-500">No messages found</p>
+          </div>
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default DashboardPage;
