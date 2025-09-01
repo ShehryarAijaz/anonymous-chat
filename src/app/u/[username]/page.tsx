803675bf98629e572e5a18e7f5422e75a09f6a16
Shehryar Aijaz <shehryar120345@gmail.com>
2025-09-02 00:22:44 +0500

    feat: Add MessageCard component to MessagePage for displaying messages

--- src/app/u/[username]/page.tsx
+++ src/app/u/[username]/page.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import MessageCard from '@/components/shared/MessageCard';
 
 const MessagePage = () => {
   return (
+    <>
     <div>Message</div>
+    <MessageCard />
+    </>
   )
 }
 
