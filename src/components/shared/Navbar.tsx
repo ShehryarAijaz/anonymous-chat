fc8d4080dc5d6d90f3bce4e97aae8e9a45c26561
Shehryar Aijaz <shehryar120345@gmail.com>
2025-09-02 00:21:44 +0500

    feat: Add ThemeModeToggle component and integrate it into Navbar for theme switching

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link'
 import { useSession, signOut } from 'next-auth/react'
 import { User } from 'next-auth'
 import { Button } from '@/components/ui/button'
+import { ThemeModeToggle } from '@/components/shared/ThemeModeToggle'
 
 const Navbar = () => {
 
@@ -11,23 +12,23 @@ const Navbar = () => {
     console.log("DATA: ", session)
     console.log("STATUS: ", status)
 
-    // TODO: do this tomorrow (going to sleep)
     const user = session?.user as User
 
   return (
-    <nav className="bg-white shadow-sm sticky top-0 z-50">
-      <div className='flex justify-between items-center max-w-5xl mx-auto px-4 py-3'>
+    <nav className="bg-white dark:bg-gray-900 shadow-sm sticky top-0 z-50">
+      <div className='flex justify-between items-center max-w-6xl mx-auto px-4 py-3'>
         <a className='text-2xl font-bold' href="#">Anonymous Chat</a>
-        {
-          session ? (
-            <>
+        {session ? (
+          <>
             <span className='text-sm text-gray-500'>Welcome, {user?.username || user?.email}</span>
             <Button onClick={() => signOut()}>Logout</Button>
+            <ThemeModeToggle />
             </>
           ) : (
             <div className='flex gap-2 items-center'>
               <Button asChild><Link href="/sign-in">Sign In</Link></Button>
               <Button asChild><Link href="/sign-up">Sign Up</Link></Button>
+              <ThemeModeToggle />
             </div>
           )
         }
