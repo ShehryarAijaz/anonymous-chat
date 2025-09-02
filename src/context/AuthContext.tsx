6b721b7a1476f604ca26dfa34d9f0a7965dcd463
Shehryar Aijaz <shehryar120345@gmail.com>
2025-09-02 21:00:02 +0500

    feat: Update AuthContext to include session refetching options for improved session management

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,7 +1,15 @@
 "use client"
 import React from "react"
 import { SessionProvider } from "next-auth/react"
- 
+
 export default function AuthContext({ children }: { children: React.ReactNode }) {
-    return <SessionProvider>{children}</SessionProvider>
+    return (
+        <SessionProvider
+            refetchInterval={5 * 60} // Refetch session every 5 minutes
+            refetchOnWindowFocus={false} // Refetch when window regains focus
+            refetchWhenOffline={false} // Don't refetch when offline
+        >
+            {children}
+        </SessionProvider>
+    )
 }
